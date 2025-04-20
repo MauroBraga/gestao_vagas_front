2c0c67d7fd644d3fd91da0e993a49a77cbf1bf37
mauro <mrb0305@gmail.com>
2025-04-20 11:08:09 -0300

    Introdução thymeleaf

--- src/main/java/br/com/mrb/gestao_vagas_front/PrimeiraPaginaController.java
+++ src/main/java/br/com/mrb/gestao_vagas_front/PrimeiraPaginaController.java
@@ -3,6 +3,7 @@ package br.com.mrb.gestao_vagas_front;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,4 +22,16 @@ public class PrimeiraPaginaController {
     public String loginCandidate() {
         return "candidate/login";
     }
+
+    @PostMapping("/create")
+    public String cadastro(Model model,Candidado candidato) {
+        System.out.println("Nome: "+candidato.nome);
+        System.out.println("Email: "+candidato.email);
+        System.out.println("Usuario: "+candidato.usuario);
+
+        model.addAttribute("candidato", candidato);
+
+        return "candidate/info";
+    }
+    record Candidado(String nome, String email, String usuario) {}
 }
