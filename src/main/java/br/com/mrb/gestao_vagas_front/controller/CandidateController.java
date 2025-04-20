0a95104f90f32bd51f97dcd3a890ad08235b0f50
mauro <mrb0305@gmail.com>
2025-04-20 14:23:43 -0300

    add sprin security

--- src/main/java/br/com/mrb/gestao_vagas_front/controller/CandidateController.java
+++ src/main/java/br/com/mrb/gestao_vagas_front/controller/CandidateController.java
@@ -1,16 +1,47 @@
 package br.com.mrb.gestao_vagas_front.controller;
 
+import br.com.mrb.gestao_vagas_front.service.CandidateService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@RequestMapping
+@RequestMapping("/candidate")
 public class CandidateController {
+
+    final
+    CandidateService candidateService;
+
+    public CandidateController(CandidateService candidateService) {
+        this.candidateService = candidateService;
+    }
 
     @GetMapping("/login")
     public String loginCandidate() {
         return "candidate/login";
+    }
+
+    @PostMapping("/signIn")
+    public String   signInCandidate(RedirectAttributes redirectAttributes,String username, String password) {
+      try{
+          var token = this.candidateService.login(username, password);
+
+          return "candidate/profile";
+
+
+      } catch (HttpClientErrorException e) {
+          redirectAttributes.addFlashAttribute("error_message", "Invalid username or password");
+          return "redirect:/candidate/login";
+      }
+    }
+
+    @GetMapping("/profile")
+    public String profile(){
+        return "candidate/profile";
     }
 
 }
---
+++ src/main/java/br/com/mrb/gestao_vagas_front/dto/Token.java
@@ -0,0 +1,13 @@
+package br.com.mrb.gestao_vagas_front.dto;
+
+public class Token {
+    private String accessToken;
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
